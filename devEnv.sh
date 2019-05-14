27f3e7049b18972cf2922c7957d26978398f2357
Shuming.fan <shuming.fan@niic.net.cn>
2019-05-14 01:32:00 +0000

    1st

---
+++ devEnv.sh
@@ -0,0 +1,2 @@
+#add libusb1.0
+export PKG_CONFIG_PATH=/home/fan/ti-processor-sdk-linux-rt-am335x-evm-05.00.00.15/linux-devkit/sysroots/armv7ahf-neon-linux-gnueabi/usr/lib/pkgconfig:$PKG_CONFIG_PATH
